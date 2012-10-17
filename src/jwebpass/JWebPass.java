22e5e2e89e6d64da2b738e70e1ef71547489c51f
Yasyf Mohamedali <yasyf@yasyf.com>
2012-10-16 18:53:15 -0700

    Externalized methods for showing prompts

--- src/jwebpass/JWebPass.java
+++ src/jwebpass/JWebPass.java
@@ -1,6 +1,9 @@
 package jwebpass;
 
 import java.io.IOException;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 
 /**
  *
@@ -8,7 +11,10 @@ import java.io.IOException;
  */
 public class JWebPass {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
+        try
+        {
+            
         //URL of passwords document -- see the below as an example
         String passLoc = "http://50.28.29.78/~omeglesp/auth.php";
         //Length of codes/passwords
@@ -17,8 +23,41 @@ public class JWebPass {
         String passDelimStart = "START";
         //Delimiter at end of passwords 
         String passDelimEnd = "END";
-        
-        WebReader pass = new WebReader(passLoc, passLength, passDelimStart,passDelimEnd);
-        System.out.println(pass.getPasswords());
+        promptResponses responses = new promptResponses();
+        WebReader pass = new WebReader(passLoc, passLength, passDelimStart, passDelimEnd);
+        //System.out.println(pass.getPasswords());
+        String entrance = responses.requestAuth();
+        if (entrance.isEmpty()) {
+            responses.failedAuth();
+        }
+        for (int i = 0; i < pass.getPasswords().size(); i++) {
+
+            if (pass.getPasswords().get(i).equals(entrance)) {
+               i = pass.getPasswords().size();
+                JOptionPane.showMessageDialog(new JDialog(),
+                        "Correct ID Entered!",
+                        "Authentication Success",
+                        JOptionPane.INFORMATION_MESSAGE);
+                 System.exit(0);
+                 
+            } 
+            else if (i == pass.getPasswords().size() - 1) {
+                JOptionPane.showMessageDialog(new JDialog(),
+                        "Incorrect ID Entered",
+                        "Authentication Failure",
+                        JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+                throw new Exception("Authentication Needed");
+            }
+
+        }
+
     }
+          catch (NullPointerException ex) {
+            
+            ex.printStackTrace();
+        }
+    }
+  
 }
+
---
+++ src/jwebpass/promptResponses.java
@@ -0,0 +1,30 @@
+package jwebpass;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+
+/**
+ *
+ * @author yasyf
+ */
+public class promptResponses {
+    
+public String requestAuth(){
+        return JOptionPane.showInputDialog(new JPasswordField(), "Enter Your ID Code", "Authentication Needed",
+                JOptionPane.WARNING_MESSAGE);
+    }
+
+    public void failedAuth() throws Exception {
+        JOptionPane.showMessageDialog(new JDialog(),
+                "Incorrect ID Entered",
+                "Authentication Failure",
+                JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+        throw new Exception("Authentication Needed");
+    }
+    
+    
+
+    
+}
